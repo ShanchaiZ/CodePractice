f822c193b5f1f4e77820117ab9d63b363fff8a2d
ShanchaiZ <shanchai.zahid@gmail.com>
2023-08-21 21:39:33 -0400

    Training JS #9: loop statement --while and do..while

--- Training9_whileAndDoWhile.js
+++ Training9_whileAndDoWhile.js
@@ -56,3 +56,25 @@
 
 // =============================================================================================================
 // Exercise:
+
+function padIt(str, n) {
+    let result = str;
+    let i = 0;
+
+    while (i < n) {
+        if (i % 2 === 0) {
+            result = `*${result}`
+        } else {
+            result = `${result}*`
+        }
+        i += 1;
+    }
+    return (result);
+}
+
+// TO call on the function use the arguments below:
+const inputString = "a";
+const asteriskCount = 10;
+
+const paddedString = padIt(inputString, asteriskCount);
+console.log(paddedString);
